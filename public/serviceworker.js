1b271e63f0bbab4771a9dcfb9bf9c4989324e990
Yann (via WeWeb) <contact@iasolutions.ch>
2026-07-13 13:27:59 +0000

    v428 - ok

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 427;
+const version = 428;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
